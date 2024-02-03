883ef5acb83d5639ccfbc1f132d1e8a91e01264a
Maciej Necka <macieknecka@gmail.com>
2024-02-03 14:26:21 +0100

    Styles fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__img-box">
-        <h1>Image here</h1>
+        <img src="#" alt="Pict here"></img>
       </div>
       <h1>Image description here</h1>
       <Nav />
